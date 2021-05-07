522600ee9200cc1181c773f721c5ba7e14f60eae
j96542 <silveira.marques@tecnico.ulisboa.pt>
2021-05-07 10:22:22 +0100

    ID=IC+IR1

--- t3/mat/script_octave.m
+++ t3/mat/script_octave.m
@@ -1,355 +1,137 @@
 pkg load symbolic
 
-file = fopen('data.txt', 'r');
-v = fscanf(file, '%f');
-fclose(file)
-
-R1 = v(1)*1000
-R2 = v(2)*1000
-R3 = v(3)*1000
-R4 = v(4)*1000
-R5 = v(5)*1000
-R6 = v(6)*1000
-R7 = v(7)*1000
-Vs = v(8)
-C = v(9)/1e6
-Kb = v(10)/1000
-Kd = v(11)*1000
-
-#{
-R1=1.04005394176e3;
-R2=2.07146823978e3;
-R3=3.06015694112e3;
-R4=4.13750728298e3;
-R5=3.13205467735e3;
-R6=2.01065636997e3;
-R7=1.00318758033e3;
-Vs=5.14514577871;
-C=1.03830911265e-6;
-Kb=7.04881622155e-3;
-Kd=8.3495605781e3; 
-#}
-Ic=0.;
 
 
-Z=0.0;
-U=1.0;
+function y = f (x)
+  c=400.e-6;
+  n=3;
+  A=230./n;
+  w=50.0*2*pi;
+  R1=1000.;
+  R2=219.834;
+  Von=0.65;
+  y = zeros (1, 1);
+  y(1) = R2*((abs(A*sin(w*x))-2.*Von)/R1+A*w*abs(cos(w*x))*c)-A*abs(sin(w*x))+2.*Von+12.;
+endfunction
 
-A2=[
-U, Z, Z, Z, Z, Z, Z;
-1/R1, -(1/R1+1/R2+1/R3), 1/R2, 1/R3, Z, Z, Z; 
-Z, 1/R2+Kb, -1/R2, -Kb, Z, Z, Z;
-Z, Z, Z, U, Z, Kd/R6, -U;
-Z, -Kb, Z, 1/R5+Kb, -1/R5, Z, Z;
-Z, Z, Z, Z, Z, -(1/R6+1/R7), 1/R7;
-Z, 1/R3, Z, -(1/R4+1/R5+1/R3), 1/R5, 1/R7, -1/R7
-];
+[x, fval, info] = fsolve (@f, 1)
 
-B2=[Vs; Z; Z; Z; Ic; Z; -Ic];
 
-X2=A2\B2;
+function t = h (x1)
+  
+  toff=0.0051;
+  c=400.e-6;
+  w=50.0*2*pi;
+  n=3;
+  A=230.0/n;
+  Von=0.65;
+  A2=A*sin(w*toff)-2*Von;
+  R1=1000.;
+  R2=219.834;
+  Req=R1*R2/(R1+R2);
+  
+  t = zeros (1, 1);
+  t(1) = A*abs(sin(w*x1))-2*Von-A2*exp(-(x1-toff)/(Req*c));
+  
+endfunction
 
-I1=(X2(1)-X2(2))/R1;
-I2=(X2(2)-X2(3))/R2;
-I3=(X2(2)-X2(4))/R3;
-I4=(-X2(4))/R4;
-I5=(X2(4)-X2(5))/R5;
-I6=(-X2(6))/R6;
+[x1, fval, info] = fsolve (@h, 0.013)
 
 
 
-Vx=X2(5)-X2(7);
+t0=0:0.0002:0.2;
+t1=0.0051:0.0001:0.013495;
+t2=0.0151:0.0001:0.023495;
+t3=0.0251:0.0001:0.033495;
+t4=0.0351:0.0001:0.043495;
+t5=0.0451:0.0001:0.053495;
+t6=0.0551:0.0001:0.063495;
+t7=0.0651:0.0001:0.073495;
+t8=0.0751:0.0001:0.083495;
+t9=0.0851:0.0001:0.093495;
+t10=0.0951:0.0001:0.103495;
+t11=0.1051:0.0001:0.113495;
+t12=0.1151:0.0001:0.123495;
+t13=0.1251:0.0001:0.133495;
+t14=0.1351:0.0001:0.143495;
+t15=0.1451:0.0001:0.153495;
+t16=0.1551:0.0001:0.163495;
+t17=0.1651:0.0001:0.173495;
+t18=0.1751:0.0001:0.183495;
+t19=0.1851:0.0001:0.193495;
 
-A=[
--(1/R1+1/R2+1/R3), 1/R2, 1/R3, Z, Z;
-1/R2+Kb, -1/R2, -Kb, Z, Z; 
-Z, Z, U, Z, -U+Kd/(R6+R7);
-Z, Z, Z, U, -U;
-1/R3-Kb, Z, -(1/R4+1/R3)+Kb, Z, 1/(R6+R7);
-];
-
-B=[Z; Z; Z; Vx; Z];
-
-X=A\B
-
-Ix=X(4)/R5;
-Req=Vx/Ix;
-
-
-w= 1.e3*2*pi
-
-A4=[
-U, Z, Z, Z, Z, Z, Z;
-1/R1, -(1/R1+1/R2+1/R3), 1/R2, 1/R3, Z, Z, Z; 
-Z, 1/R2+Kb, -1/R2, -Kb, Z, Z, Z;
-Z, Z, Z, U, Z, Kd/R6, -U;
-Z, -Kb, Z, 1/R5+Kb, -1/R5-i*w*C, Z, i*w*C;
-Z, Z, Z, Z, Z, -(1/R6+1/R7), 1/R7;
-Z, 1/R3, Z, -(1/R4+1/R5+1/R3), 1/R5+i*w*C, 1/R7, -1/R7-i*w*C
-];
-
-B4=[1;Z;Z;Z;Z;Z;Z];
-
-X4=A4\B4
-
-
-
-
-%time axis: 0 to 20ms with 1us steps
-t=0:1e-6:20e-3; %s
-
-#Condição inicial com o ground em V4
-
-v6=Vx*exp(-t/(Req*C));
-
-hf = figure ();
-plot (t, v6, "g");
-hold on;
-legend("V6n","location","northeastoutside");
-xlabel ("t[s]");
-ylabel ("v6(t) [V]");
-hold off
-print (hf, "natural.eps", "-depsc");
-
-
-
-%time axis: -5 to 0ms with 1us steps
-t2=-5e-3:1e-6:0;
-
-vn=Vx*exp(-t/(Req*C));
-vf=abs(X4(5))*sin(w*t+arg(X4(5)));
-vt=vn+vf;
-vi=Vx+0*t2;
-
-
-vsp=cos(w*t-pi/2);
-vsn=Vs+0*t2;
-
-h2 = figure ();
-plot (t, vt, "g3");
-hold on;
-plot (t2, vi, "g3");
-#hold on;
-plot (t, vsp, "g2");
-#hold on;
-plot (t2, vsn, "g2");
-
-legend("V6","V6","Vs","Vs","location","northeastoutside");
-xlabel ("t[s]");
-ylabel ("v6(t), vs(t) [V],");
-hold off
-print (h2, "total.eps", "-depsc");
-
-
-%time axis: -1 to 6 with 1us steps
-
-t3=ones(71);
-
-V6f = ones(71);
-V6m = ones(71);
-
-Vcf = ones(71);
-Vcm = ones(71);
-
-
-#f1=10.^(t3);
-
-for i = 0:70
-
-f = 10^(i*1e-1 - 1);
-
-t3(i+1) = i*1e-1 - 1;
-
-w1= f*2*pi;
-
-Afreq=[
-U, Z, Z, Z, Z, Z, Z;
-1/R1, -(1/R1+1/R2+1/R3), 1/R2, 1/R3, Z, Z, Z; 
-Z, 1/R2+Kb, -1/R2, -Kb, Z, Z, Z;
-Z, Z, Z, U, Z, Kd/R6, -U;
-Z, -Kb, Z, 1/R5+Kb, -1/R5-j*w1*C, Z, j*w1*C;
-Z, Z, Z, Z, Z, -(1/R6+1/R7), 1/R7;
-Z, 1/R3, Z, -(1/R4+1/R5+1/R3), 1/R5+j*w1*C, 1/R7, -1/R7-j*w1*C
-];
-
-Bfreq=[1;Z;Z;Z;Z;Z;Z];
-
-Xfreq=Afreq\Bfreq;
-
-V6f(i+1) = arg(Xfreq(5));
-V6m(i+1) = abs(Xfreq(5));
-
-Vcf(i+1) = arg(Xfreq(5) - Xfreq(7));
-Vcm(i+1) = abs(Xfreq(5) - Xfreq(7));
-
-endfor
-
-faseVs=0*t3;
-
-
-
-h3 = figure ();
-plot (t3, V6f*180/pi, "g5");
-hold on;
-plot (t3, faseVs*180/pi, "g3");
-#hold on;
-plot (t3, Vcf*180/pi, "g4");
-
-legend("V6","Vs(dark)","VC(pink)", "location","northeastoutside");
-
-xlabel ("f[ln(Hz)]");
-ylabel ("phase [degrees],");
-hold off
-print (h3, "phase.eps", "-depsc");
-
-
-#TV6=20*log10(abs(-Kb*X4(2) + (1/R5+Kb)*X4(4) + i*2*pi*f*C*X4(7)))-20*log10(abs(1/R5+i*2*pi*f*C))
-
-#TV8 = 20*log10(abs(X4(7)));
-
-TVs=20*log10(1+0*t3);
-
-#TVc=20*log10(abs(VV6-X4(7)))
+w=50.0*2*pi;
+Von=0.65;
+toff=0.0051;
+A2=230/3*sin(w*toff)-2*Von;
+R1=1000.;
+R2=219.834;
+Req=R1*R2/(R1+R2);
+c=400.e-6;
 
 h4 = figure ();
-plot (t3, 20*log10(V6m), "g5");
+plot (t0, abs(230/3*sin(w*t0))-2*Von, "g0");
 hold on;
-plot (t3, TVs, "g3");
-#hold on;
-plot (t3, 20*log10(Vcm), "g4");
+plot (t1, A2*exp(-(t1-t1(1))/(Req*c)), "g1");
+hold on;
+plot (t2, A2*exp(-(t2-t2(1))/(Req*c)), "g1");
+hold on;
+plot (t3, A2*exp(-(t3-t3(1))/(Req*c)), "g1");
+hold on;
+plot (t4, A2*exp(-(t4-t4(1))/(Req*c)), "g1");
+hold on;
+plot (t5, A2*exp(-(t5-t5(1))/(Req*c)), "g1");
+hold on;
+plot (t6, A2*exp(-(t6-t6(1))/(Req*c)), "g1");
+hold on;
+plot (t7, A2*exp(-(t7-t7(1))/(Req*c)), "g1");
+hold on;
+plot (t8, A2*exp(-(t8-t8(1))/(Req*c)), "g1");
+hold on;
+plot (t9, A2*exp(-(t9-t9(1))/(Req*c)), "g1");
+hold on;
+plot (t10, A2*exp(-(t10-t10(1))/(Req*c)), "g1");
+hold on;
+plot (t11, A2*exp(-(t11-t11(1))/(Req*c)), "g1");
+hold on;
+plot (t12, A2*exp(-(t12-t12(1))/(Req*c)), "g1");
+hold on;
+plot (t13, A2*exp(-(t13-t13(1))/(Req*c)), "g1");
+hold on;
+plot (t14, A2*exp(-(t14-t14(1))/(Req*c)), "g1");
+hold on;
+plot (t15, A2*exp(-(t15-t15(1))/(Req*c)), "g1");
+hold on;
+plot (t16, A2*exp(-(t16-t16(1))/(Req*c)), "g1");
+hold on;
+plot (t17, A2*exp(-(t17-t17(1))/(Req*c)), "g1");
+hold on;
+plot (t18, A2*exp(-(t18-t18(1))/(Req*c)), "g1");
+hold on;
+plot (t19, A2*exp(-(t19-t19(1))/(Req*c)), "g1");
 
-#h={'V6','Vs','VC'}
 legend("V6","Vs(dark)","VC(pink)", "location","northeastoutside");
 
 xlabel ("f[ln(Hz)]");
 ylabel ("Magnitude [dB],");
+
 hold off
+
 print (h4, "magnitude.eps", "-depsc");
 
 
-printf("op2_TAB\n")
-printf("$\\widetilde{V_1}$ = %.15e + i%.15e\n", real(X4(1)), imag(X4(1)))
-printf("$\\widetilde{V_2}$ = %.15e + i%.15e\n", real(X4(2)), imag(X4(2)))
-printf("$\\widetilde{V_3}$ = %.15e + i%.15e\n", real(X4(3)), imag(X4(3)))
-printf("$\\widetilde{V_5}$ = %.15e + i%.15e\n", real(X4(4)), imag(X4(4)))
-printf("$\\widetilde{V_6}$ = %.15e + i%.15e\n", real(X4(5)), imag(X4(5)))
-printf("$\\widetilde{V_7}$ = %.15e + i%.15e\n", real(X4(6)), imag(X4(6)))
-printf("$\\widetilde{V_8}$ = %.15e + i%.15e\n", real(X4(7)), imag(X4(7)))
-printf("op2_END\n")
 
-printf("op1_TAB\n")
-printf("$V_1$ = %.15e\n", X2(1))
-printf("$V_2$ = %.15e\n", X2(2))
-printf("$V_3$ = %.15e\n", X2(3))
-printf("$V_5$ = %.15e\n", X2(4))
-printf("$V_6$ = %.15e\n", X2(5))
-printf("$V_7$ = %.15e\n", X2(6))
-printf("$V_8$ = %.15e\n", X2(7))
-printf("$R1[i]$ = %.15e\n", I1)
-printf("$R2[i]$ = %.15e\n", I2)
-printf("$R3[i]$ = %.15e\n", I3)
-printf("$R4[i]$ = %.15e\n", I4)
-printf("$R5[i]$ = %.15e\n", I5)
-printf("$Id$ = %.15e\n", I6)
-printf("op1_END\n")
+h = figure ();
 
-printf("op_TAB\n")
-printf("$V_x$ = %.15e V\n", Vx)
-printf("$V_6$ = %.15e V\n", X(4))
-printf("$V_8$ = %.15e V\n", X(5))
-printf("$I_x$ = %.15e A\n", Ix)
-printf("$R_eq$ = %.15e $\\Omega$\n", Req)
-printf("op_END\n")
+ampl=71.99;
+rd=27.552;
+k=18*rd/(18*rd+R2)
 
-
-spice1 = fopen("spice1.cir", "w");
-
-#fprintf(spice1, ".OP\n");
-fprintf(spice1, "R1 1 2 %f\n", R1);
-fprintf(spice1, "R2 2 3 %f\n", R2);
-fprintf(spice1, "R3 2 5 %f\n", R3);
-fprintf(spice1, "R4 0 5 %f\n", R4);
-fprintf(spice1, "R5 5 6 %f\n", R5);
-fprintf(spice1, "R6 9 7 %f\n", R6);
-fprintf(spice1, "R7 7 8 %f\n", R7);
-
-fprintf(spice1, "Vs 1 0 %f\n", Vs);
-
-fprintf(spice1, "V0 0 9 0V\n");
-fprintf(spice1, "Hd 5 8 V0 %f\n", Kd);
-
-fprintf(spice1, "Gb 6 3 (2,5) %f\n", Kb);
-
-fclose(spice1);
-
-
-spice2 = fopen("spice2.cir", "w");
-
-fprintf(spice2, "R1 1 2 %f\n", R1);
-fprintf(spice2, "R2 2 3 %f\n", R2);
-fprintf(spice2, "R3 2 5 %f\n", R3);
-fprintf(spice2, "R4 0 5 %f\n", R4);
-fprintf(spice2, "R5 5 6 %f\n", R5);
-fprintf(spice2, "R6 9 7 %f\n", R6);
-fprintf(spice2, "R7 7 8 %f\n", R7);
-
-fprintf(spice2, "Vs 1 0 0V\n");
-fprintf(spice2, "V0 0 9 0V\n");
-fprintf(spice2, "Vx 6 8 %f\n", Vx);
-
-fprintf(spice2, "Hd 5 8 V0 %f\n", Kd);
-fprintf(spice2, "Gb 6 3 (2,5) %f\n", Kb);
-
-fclose(spice2);
+plot (t0, k*(abs(230/3*sin(w*t0))-2*Von-ampl)+11.7, "g0");
+hold on;
+plot (t1, k*(A2*exp(-(t1-t1(1))/(Req*c))-ampl)+11.7, "g1");
+hold on;
 
 
 
-spice3 = fopen("spice3.cir", "w");
-
-fprintf(spice3, "R1 1 2 %f\n", R1);
-fprintf(spice3, "R2 2 3 %f\n", R2);
-fprintf(spice3, "R3 2 5 %f\n", R3);
-fprintf(spice3, "R4 0 5 %f\n", R4);
-fprintf(spice3, "R5 5 6 %f\n", R5);
-fprintf(spice3, "R6 9 7 %f\n", R6);
-fprintf(spice3, "R7 7 8 %f\n", R7);
-
-fprintf(spice3, "Vs 1 0 0V\n");
-fprintf(spice3, "V0 0 9 0V\n");
-
-fprintf(spice3, "Hd 5 8 V0 %f\n", Kd);
-fprintf(spice3, "Gb 6 3 (2,5) %f\n", Kb);
-
-fprintf(spice3, "C1 6 8 %f\n", C);
-
-#X(5) msm??
-fprintf(spice3, ".ic V(6)=%f V(8)=%f\n", X(4), X(5)); 
-
-fclose(spice3);
-
-spice45 = fopen("spice45.cir", "w");
-
-fprintf(spice45, "R1 1 2 %f\n", R1);
-fprintf(spice45, "R2 2 3 %f\n", R2);
-fprintf(spice45, "R3 2 5 %f\n", R3);
-fprintf(spice45, "R4 0 5 %f\n", R4);
-fprintf(spice45, "R5 5 6 %f\n", R5);
-fprintf(spice45, "R6 9 7 %f\n", R6);
-fprintf(spice45, "R7 7 8 %f\n", R7);
-
-fprintf(spice45, "Vs 1 0 0.0 ac 1.0 sin(0 1 1k)\n");
-%fprintf(spice45, "Vs 1 0 0.0 ac 1.0 sin(0 1.0 1.0)\n");
-fprintf(spice45, "V0 0 9 0V\n");
-
-fprintf(spice45, "Hd 5 8 V0 %f\n", Kd);
-fprintf(spice45, "Gb 6 3 (2,5) %f\n", Kb);
-
-fprintf(spice45, "C1 6 8 %f\n", C);
-
-#X(5) msm??
-fprintf(spice45, ".ic V(6)=%f V(8)=%f\n", X(4), X(5)); 
-
-fclose(spice45);
-
-
-
+print (h, "V2.eps", "-depsc");
